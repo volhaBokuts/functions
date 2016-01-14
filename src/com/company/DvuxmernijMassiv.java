1afec0cecb1d868f0f8b106e5938b70ca1e8e9a2
softmartian <softmartian@gmail.com>
2016-01-14 18:52:54 +0300

    dvuxmernij massiv

---
+++ src/com/company/DvuxmernijMassiv.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class DvuxmernijMassiv {
+
+    public static void main(String[] args) {
+
+        int[][] mas = new int[10][5];
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                mas[i][j] = 1;
+            }
+        }
+
+    }
+
+}
